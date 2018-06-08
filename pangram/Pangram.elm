a7da52e7405da576997c10e5426c4c2e11dc2ee3
Oleg Okunevych <okunevichov@gmail.com>
2018-06-08 16:02:06 +0300

    Implement Pangram solution

--- pangram/Pangram.elm
+++ pangram/Pangram.elm
@@ -1,6 +1,26 @@
 module Pangram exposing (isPangram)
 
+import Array exposing (..)
+import Set exposing (..)
+
+initialAlphabet : Set String
+initialAlphabet =
+    Set.fromList ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+isSetSizeEqual : Set a -> Set b -> Bool
+isSetSizeEqual a b =
+    Set.size a == Set.size b
+
+intersectWithAlphabet : Set String -> Set String
+intersectWithAlphabet sentence =
+    sentence
+    |> Set.intersect initialAlphabet
 
 isPangram : String -> Bool
 isPangram sentence =
-    Debug.crash "Please implement this function"
+    sentence
+    |> String.toUpper
+    |> String.split ""
+    |> Set.fromList
+    |> intersectWithAlphabet
+    |> isSetSizeEqual initialAlphabet
