b0213536df135bec3caced4de59dc994e805fae9
Oleg Okunevych <okunevichov@gmail.com>
2018-06-14 17:59:53 +0300

    Add anagram task solution

--- anagram/Anagram.elm
+++ anagram/Anagram.elm
@@ -1,10 +1,24 @@
 module Anagram exposing (detect)
 
-compareWith : List String -> String -> List String
-compareWith candidates word =
-    functionBody
+import Char exposing (..)
+
+isAnagram : String -> String -> Bool
+isAnagram candidateWord word =
+    String.toUpper candidateWord /= String.toUpper word
+    && (
+        -- Handles detecting of unicode anagrams
+        transformWord (String.toLower candidateWord) ==
+        transformWord (String.toLower word)
+        || transformWord candidateWord ==
+        transformWord word
+    )
+
+transformWord : String -> List Char
+transformWord word =
+    word
+    |> String.toList
+    |> List.sort 
 
 detect : String -> List String -> List String
 detect word candidates =
-    candidates
-    |> compareWith word
+    List.filter (\ candidateWord -> isAnagram candidateWord word) candidates
