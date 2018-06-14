f6dcbc0ed5fb5642e9f32fc76f3c26b880f60967
Oleg Okunevych <okunevichov@gmail.com>
2018-06-14 18:39:37 +0300

    Space Age task solution

--- space-age/SpaceAge.elm
+++ space-age/SpaceAge.elm
@@ -1,6 +1,5 @@
 module SpaceAge exposing (Planet(..), ageOn)
 
-
 type Planet
     = Mercury
     | Venus
@@ -11,7 +10,19 @@ type Planet
     | Uranus
     | Neptune
 
+orbitalPeriod : Planet -> Float
+orbitalPeriod planet =
+    case planet of
+        Mercury -> (orbitalPeriod Earth) * 0.2408467
+        Venus -> (orbitalPeriod Earth) * 0.61519726
+        Mars -> (orbitalPeriod Earth) * 1.8808158
+        Jupiter -> (orbitalPeriod Earth) * 11.862615
+        Saturn -> (orbitalPeriod Earth) * 29.447498
+        Uranus -> (orbitalPeriod Earth) * 84.016846
+        Neptune -> (orbitalPeriod Earth) * 164.79132
+        Earth -> 31557600
+
 
 ageOn : Planet -> Float -> Float
 ageOn planet seconds =
-    Debug.crash "Please implement this function"
+    seconds / orbitalPeriod(planet)
