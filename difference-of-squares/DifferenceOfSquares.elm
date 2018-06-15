e34653871898622e2b599fc563c98552c0856c41
Oleg Okunevych <okunevichov@gmail.com>
2018-06-15 13:54:57 +0300

    Difference of squares task

---
+++ difference-of-squares/DifferenceOfSquares.elm
@@ -0,0 +1,16 @@
+module DifferenceOfSquares exposing (difference, squareOfSum, sumOfSquares)
+
+
+squareOfSum : Int -> Int
+squareOfSum n =
+    Debug.crash "Please implement this function"
+
+
+sumOfSquares : Int -> Int
+sumOfSquares n =
+    Debug.crash "Please implement this function"
+
+
+difference : Int -> Int
+difference n =
+    Debug.crash "Please implement this function"
