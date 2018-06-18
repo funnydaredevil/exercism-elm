7de31fd1f1cbe15f615dd6631ba2fb5f99d695a7
Oleg Okunevych <okunevichov@gmail.com>
2018-06-18 14:43:48 +0300

    Word count solution

--- word-count/WordCount.elm
+++ word-count/WordCount.elm
@@ -1,6 +1,21 @@
 module WordCount exposing (wordCount)
 
+import Dict exposing (..)
+import Regex exposing (..)
 
 wordCount : String -> Dict String Int
 wordCount sentence =
-    Debug.crash "Please implement this function"
+    sentence
+    |> String.toLower
+    |> removePunctuation
+    |> String.words
+    |> List.foldr (\w d -> Dict.update w incrOrInit d) Dict.empty
+
+removePunctuation : String -> String
+removePunctuation word =
+    word
+    |> replace All (regex "\\W+") (\_ -> " ")
+
+incrOrInit : Maybe Int -> Maybe Int
+incrOrInit maybe =
+    Just <| (Maybe.withDefault 0 maybe) + 1
