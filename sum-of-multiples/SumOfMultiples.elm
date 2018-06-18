056bde49e21c6d7f6a1b00923bd14b9ac5bd1aec
Oleg Okunevych <okunevichov@gmail.com>
2018-06-18 16:25:49 +0300

    Sum of multiples solution

--- sum-of-multiples/SumOfMultiples.elm
+++ sum-of-multiples/SumOfMultiples.elm
@@ -1,6 +1,19 @@
 module SumOfMultiples exposing (sumOfMultiples)
 
+import Set exposing (..)
+
+findMultiples : Int -> Int -> List Int
+findMultiples number multiple =
+    number - 1
+    |> List.range 1
+    |> List.filter (\n -> rem n multiple == 0)
+
+sumUniqueValues : List Int -> Int
+sumUniqueValues list =
+    list |> Set.fromList |> Set.toList |> List.sum
 
 sumOfMultiples : List Int -> Int -> Int
 sumOfMultiples multiples limit =
-    Debug.crash "Please implement this function"
+    multiples
+    |> List.map (\n -> findMultiples limit n)
+    |> List.concat |> sumUniqueValues
