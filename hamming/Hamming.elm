922490e6ff031e350618f24898026b7a906df7f8
Oleg Okunevych <okunevichov@gmail.com>
2018-06-20 11:59:05 +0300

    Solution for hamming

--- hamming/Hamming.elm
+++ hamming/Hamming.elm
@@ -1,6 +1,28 @@
 module Hamming exposing (distance)
 
+validateLengths : String -> String -> Bool
+validateLengths left right =
+    String.length left == String.length right
+
+filterEqual : Char -> Char -> Bool
+filterEqual left right =
+    left == right
+
+countMatching : (a -> Bool) -> List a -> Int
+countMatching func listBools =
+    List.filter func listBools
+    |> List.length
+
+isFalse : Bool -> Bool
+isFalse a =
+    a == False
 
 distance : String -> String -> Result String Int
 distance left right =
-    Debug.crash "Please implement this function"
+    case validateLengths left right of
+        False ->
+            Err "left and right strands must be of equal length"
+        True ->
+            List.map2 filterEqual (String.toList left) (String.toList right)
+            |> countMatching isFalse
+            |> Ok
