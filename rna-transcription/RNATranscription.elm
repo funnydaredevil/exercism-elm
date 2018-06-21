4786b55de7cacc976900a2e43631dfaa62257720
Oleg Okunevych <okunevichov@gmail.com>
2018-06-21 22:41:59 +0300

    RNA Transcription wip

--- rna-transcription/RNATranscription.elm
+++ rna-transcription/RNATranscription.elm
@@ -1,6 +1,16 @@
 module RNATranscription exposing (toRNA)
 
+convert : Char -> String
+convert input =
+    case input of
+        'G' -> "C"
+        'C' -> "G"
+        'T' -> "A"
+        'A' -> "U"
 
 toRNA : String -> Result Char String
 toRNA dna =
-    Debug.crash "Please implement this function"
+    String.split "" dna
+    |> List.map (\n -> convert n)
+    |> String.join
+    |> Ok
