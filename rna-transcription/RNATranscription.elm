c21b4224eae9c36f3e17097027646d16d5780a0d
Oleg Okunevych <okunevichov@gmail.com>
2018-06-22 14:28:53 +0300

    RNA Transcription solution

--- rna-transcription/RNATranscription.elm
+++ rna-transcription/RNATranscription.elm
@@ -1,16 +1,17 @@
 module RNATranscription exposing (toRNA)
 
-convert : Char -> String
+convert : String -> String
 convert input =
     case input of
-        'G' -> "C"
-        'C' -> "G"
-        'T' -> "A"
-        'A' -> "U"
+        "G" -> "C"
+        "C" -> "G"
+        "T" -> "A"
+        "A" -> "U"
+        _ -> ""
 
 toRNA : String -> Result Char String
 toRNA dna =
     String.split "" dna
     |> List.map (\n -> convert n)
-    |> String.join
+    |> String.concat
     |> Ok
