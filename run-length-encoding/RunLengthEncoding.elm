4c485aaf752036a912dacf709164ea6c947ad568
Oleg Okunevych <okunevichov@gmail.com>
2018-06-24 15:52:05 +0300

    Run length encoding task

---
+++ run-length-encoding/RunLengthEncoding.elm
@@ -0,0 +1,15 @@
+module RunLengthEncoding exposing (decode, encode, version)
+
+
+version =
+    2
+
+
+encode : String -> String
+encode string =
+    Debug.crash "Please implement this function"
+
+
+decode : String -> String
+decode string =
+    Debug.crash "Please implement this function"
